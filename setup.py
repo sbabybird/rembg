a3c2d6242ce2a38faf97ae7b7f758669229b047e
sbabybird <sbabybird@gmail.com>
2023-02-09 17:03:45 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+import pathlib
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+from setuptools import find_packages, setup
+
+import versioneer
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+with open("requirements.txt") as f:
+    requireds = f.read().splitlines()
+
+with open("requirements-gpu.txt") as f:
+    gpu_requireds = f.read().splitlines()
+
+setup(
+    name="rembg",
+    description="Remove image background",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danielgatis/rembg",
+    author="Daniel Gatis",
+    author_email="danielgatis@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="remove, background, u2net",
+    packages=["rembg"],
+    python_requires="~=3.9.0",
+    install_requires=requireds,
+    entry_points={
+        "console_scripts": [
+            "rembg=rembg.cli:main",
+        ],
+    },
+    extras_require={
+        "gpu": gpu_requireds,
+    },
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
